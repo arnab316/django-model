2e28e0b46ee282532a0b164511d9f726c6d7f2a4
Arnab Golder <arnabgolder7@gmail.com>
2024-07-12 20:41:57 +0530

    django models and migrationj

---
+++ myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here
+
+
+class menu(models.Model):
+    name = models.CharField(max_length = 50)
+    cuisine = models.CharField(max_length = 100)
+    price = models.IntegerField()
+
+def __str__(self):
+    return self.name + " : " + self.cuisine
+
+
